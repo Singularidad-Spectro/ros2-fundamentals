b08cf6ff53a54ebd9efa899952cafe9f412c6a5d
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2025-05-22 16:46:44 -0500

    segundo commit c++ robotism

--- constanza/cpp-fundamentals/sensor_reader.cpp
+++ constanza/cpp-fundamentals/sensor_reader.cpp
@@ -5,6 +5,23 @@ using namespace std;
 
 
 int main(){
- cout <<"Sensor Reading is 50"
-<<endl;
+
+    int sensorReading[5] = {35,44,35,64,54};
+    int sum = 0;    int average = 0;
+
+for (int i = 0; i < 5; i++){
+    sum += sensorReading[i];
+}
+cout << "Sensor Reading sum is " << sum << endl;
+
+    average = sum / 5;
+    cout << "Sensor Reading average is " << average << endl;
+
+    if (average > 30){
+        cout << "Sensor Reading is crossing threshold" << endl;
+    }
+    else{
+        cout << "Sensor Reading is not crossing threshold" << endl;
+    }
+
 }
